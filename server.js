5ca91ae063fcf221ee49ff265b5eb50c0b2c543c
whiz123 <azeezlekan301@gmail.com>
2026-04-19 13:51:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -534,7 +534,7 @@ app.get("/fund-wallet", async (req, res) => {
   try {
     const reference = "ref_" + Date.now();
 
-    const response = await fetch("https://api.korapay.com/merchant/api/v1/charges", {
+    const response = await fetch("https://api.korapay.com/merchant/api/v1/charges/initialize", {
   method: "POST",
   headers: {
     Authorization: `Bearer ${process.env.KORAPAY_SECRET_KEY}`,
